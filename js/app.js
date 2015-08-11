ed723b52e9a0a8392fcf25950e80053df253048d
Michael <michaelyang776@gmail.com>
2015-08-11 21:45:10 +0000

    fix typo

--- js/app.js
+++ js/app.js
@@ -14,23 +14,23 @@ var frontEndUdacityProjectsData = [{
 }, {
 	name: 'Project 3: Classic Arcade Clone',
 	url: 'https://github.com/micyang/frontend-nanodegree-arcade-game',
-	description: 'Build simple frogger clone using javascript object oriented programming',
-	learningExperience: 'Udacity provided visual assets and an engine for the game to run on. It was the first time using javascript pseudo-classical object oriented programming. I created classes for player, enemies, and items. One of the major challenges was understanding scoping. I had a problem using "this" in one of my functions that I did not realize at the time but fixed it assigning "this" to a variable self.'
+	description: 'Build simple frogger clone using JavaScript object oriented programming',
+	learningExperience: 'Udacity provided visual assets and an engine for the game to run on. It was the first time using JavaScript pseudo-classical object oriented programming. I created classes for player, enemies, and items. One of the major challenges was understanding scoping. I had a problem using "this" in one of my functions that I did not realize at the time but fixed it assigning "this" to a variable self.'
 }, {
 	name: 'Project 4: Website Optimization',
 	url: 'https://github.com/micyang/frontend-nanodegree-mobile-portfolio',
 	description: 'Optimize one website to have Google speed score of 90 or higher and another website to have 60 frames per second',
-	learningExperience: 'Udacity provided sample websites for optimization. Learned about how reducing Critical Rendering Path (CRP) to increase the page load speed. By inlining and calling files asynchronously, it increases page load speed. To optimize the page to 60 frames per second, it is necessary to identify caluations that are redundant and the amount of times it calls the DOM. Accessing the DOM frequently will cause a slow down, therefore assigning a DOM element to a variable will increase the frame rate.'
+	learningExperience: 'Udacity provided sample websites for optimization. Learned about how reducing Critical Rendering Path (CRP) to increase the page load speed. By inlining and calling files asynchronously, it increases page load speed. To optimize the page to 60 frames per second, it is necessary to identify calculations that are redundant and the amount of times it calls the DOM. Accessing the DOM frequently will cause a slow down, therefore assigning a DOM element to a variable will increase the frame rate.'
 }, {
 	name: 'Project 5: Neighborhood Map',
 	url: 'https://github.com/micyang/neighborhood-map',
 	description: 'Build one page application of google maps',
-	learningExperience: 'One of the main things I learned was good software design by seperating model and view with viewModel. By separating concerns, editing data will not interfere with how the view is displayed and vice versa. I learned how to explore frameworks such as Google Maps and Yelp. The major challenge is figuring out to build this application from scratch. Udacity did not provide any sort of code base for this.'
+	learningExperience: 'One of the main things I learned was good software design by seperating model and view with viewmodel. By seperating concerns, editing data will not interfere with how the view is displayed and vice versa. I learned how to explore frameworks such as Google Maps and Yelp. The major challenge is figuring out to build this application from scratch. Udacity did not provide any sort of code base for this.'
 }, {
 	name: 'Project 6: Feed Reader',
 	url: 'https://github.com/micyang/frontend-nanodegree-feedreader',
 	description: 'Write tests for a website',
-	learningExperience: 'Udacity provided a website for testing using a javascript testing framework, Jasmine. I learned about test driven software development. Writing the tests were straight forward, until the tests involving asynchronous data. The challenge was figuring out when exactly the data is gathered. For a while I had a problem with the for loop. I thought I could use the variable i in the for loop to increment the index but it turns out that I only get the last i value. In order to increment the indexI had to use the beforeEach and afterEach functions due to the asynchronous nature.'
+	learningExperience: 'Udacity provided a website for testing using a JavaScript testing framework, Jasmine. I learned about test driven software development. Writing the tests were straight forward, until the tests involving asynchronous data. The challenge was figuring out when exactly the data is gathered. For a while I had a problem with the for loop. I thought I could use the variable i in the for loop to increment the index but it turns out that I only get the last i value. In order to increment the indexI had to use the beforeEach and afterEach functions due to the asynchronous nature.'
 }];
 
 var ViewModel = function() {
